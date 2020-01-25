fb59387fff57ea3d806defa3e8300ef7ae52ce4c
Senbagaraman <senbagaraman04@gmail.com>
2020-01-25 14:56:02 +0530

    Vehicle Details: Sample Data

--- fleetManagement/src/app/admin-panel/vehicle-details/vehicle-details.component.ts
+++ fleetManagement/src/app/admin-panel/vehicle-details/vehicle-details.component.ts
@@ -1,4 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTableModule, MatTableDataSource} from '@angular/material'
+import { DataSource } from '@angular/cdk/table';
+import { NgModule } from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import { FormControl } from '@angular/forms';
+
+
+
+export interface VEHICLE {  
+  vehicleNumber: number; //Last four number is enough
+  vehicleName: string; //Easily Identifieable name, which can be used for testing also
+  dateOfEntry: string; 
+  city:string;
+  driverName: string;
+  driverNumber: number;
+  startingMeter: number; 
+  endingMeter: number;
+  totalToday: number;
+ 
+}
+const vehicle_data: VEHICLE[] = [
+  {   
+       vehicleNumber: 1001, 
+       vehicleName: "Ace", 
+       dateOfEntry: "12/12/12",
+       city:'salem',
+       driverName:"testDriver",
+       driverNumber: 123454,
+       startingMeter:10,
+       endingMeter:100,
+       totalToday: 0
+  },   
+  {   
+    vehicleNumber: 100, 
+    vehicleName: "Ace2", 
+    dateOfEntry: "2/12/12",
+    city:'salem',
+    driverName:"testDriver",
+    driverNumber: 123354,
+    startingMeter:10,
+    endingMeter:10,
+    totalToday: 0
+},   
+
+];
 
 @Component({
   selector: 'app-vehicle-details',
@@ -7,11 +52,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleDetailsComponent implements OnInit {
 
-  constructor() { }
+  //Display's the allowed column names
+  displayedColumns: string[] = ['number', 'name', 'date', 'city', 'drivername','drivernumber','start','end','total'];
+  dataSource = new MatTableDataSource(vehicle_data);
+  positionFilter = new FormControl();
+  nameFilter = new FormControl();
+  private filterValues = {  name: '' }
+
+  filteredValues = {
+     name: '', topFilter: false
+  };
+
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  
+  //public dataSource: any
+  constructor() {
+   }
 
   ngOnInit() {
-    console.log("vehicle details loaded");
-  
+    console.log("vehicle details loaded"); 
+    this.nameFilter.valueChanges
+      .subscribe(value => {
+        this.filterValues['name'] = value
+        this.dataSource.filter = JSON.stringify(this.filterValues)
+      });
+    this.dataSource.sort = this.sort;  
   }
 
+ 
 }
--- fleetManagement/src/app/app.module.ts
+++ fleetManagement/src/app/app.module.ts
@@ -8,9 +8,14 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { RouterModule, Routes } from '@angular/router'
 import { NgVerticalTimelineModule  } from 'ng-vertical-timeline';
 
-import { MatCardModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { VehicleDetailsComponent } from './admin-panel/vehicle-details/vehicle-details.component';
+import {MatTableModule} from '@angular/material'
+import { DataSource } from '@angular/cdk/table';
+import {CdkTableModule} from '@angular/cdk/table'
+import {MatSortModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 const appRoutes: Routes = [
@@ -40,7 +45,15 @@ const appRoutes: Routes = [
     NgwWowModule,
     RouterModule,
     MatCardModule,
+    MatTableModule,
     NgVerticalTimelineModule,
+    CdkTableModule,
+    MatSortModule,
+    FormsModule, ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
     RouterModule.forRoot(appRoutes)
 
   ],
