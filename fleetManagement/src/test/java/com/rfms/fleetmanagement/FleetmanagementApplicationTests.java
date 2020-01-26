d2a8d7f9615f6d9ad94ae8d619d0d5978dcdcb42
Senbagaraman <senbagaraman04@gmail.com>
2020-01-26 11:03:57 +0530

    initial checkin - backend code

---
+++ fleetManagement/src/test/java/com/rfms/fleetmanagement/FleetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfms.fleetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
