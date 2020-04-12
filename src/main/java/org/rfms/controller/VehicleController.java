b82c45c5576062c403d55e61c6b6f0ea86e97bdb
Senbagaraman <senbagaraman04@gmail.com>
2020-04-12 09:40:13 +0530

    #fix: deployment issues

--- src/main/java/org/rfms/controller/VehicleController.java
+++ src/main/java/org/rfms/controller/VehicleController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/vehicle")
+@RestController("/vehicle")
 public class VehicleController {
 
 	@Autowired
