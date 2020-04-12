3fadbceee47a82c1ef50d5334e150b1b3721a1fb
Senbagaraman <senbagaraman04@gmail.com>
2020-04-12 09:58:32 +0530

    #Fix for dialect error

--- src/main/java/org/rfms/controller/VehicleController.java
+++ src/main/java/org/rfms/controller/VehicleController.java
@@ -13,10 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/vehicle")
+@RestController
+@RequestMapping("/vehicle")
 public class VehicleController {
 
 	@Autowired
