5c362984f5161e8380f1f0f0f0fd7c520779912c
Senbagaraman <senbagaraman04@gmail.com>
2020-06-12 22:28:46 +0530

    side-nav redesign #63

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.scss']
+  styleUrls: ['./admin-panel.component.scss'],
+  animations: [
+    trigger('expandMenuBar', [
+      state('open', style({
+        minWidth: '270px',
+        maxWidth: '299px',
+        zIndex: '11',
+      })),
+      state('closed', style({
+        width: '64px',
+        overflow: 'hidden',
+        minHeight: '100vw'
+      })),
+      transition('open => closed', [
+        animate('0s'),
+      ]),
+      transition('closed => open', [
+        animate('0s')
+      ])
+    ])
+  ]
 })
 export class AdminPanelComponent implements OnInit {
-
+ 
+  expandMenu : boolean = false;
   constructor() { }
 
   ngOnInit() {
