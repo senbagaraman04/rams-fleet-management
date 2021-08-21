a7cc265d983852057dd0b3961fcf498a16795f06
Senbagaraman <senbagaraman.manoharan@siemens.com>
2021-08-21 17:54:25 +0530

    fix: code cleanup

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,30 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-
+ 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.scss'],
-  animations: [
-    trigger('expandMenuBar', [
-      state('open', style({
-        minWidth: '270px',
-        maxWidth: '299px',
-        zIndex: '11',
-      })),
-      state('closed', style({
-        width: '64px',
-        overflow: 'hidden',
-        minHeight: '100vw'
-      })),
-      transition('open => closed', [
-        animate('0s'),
-      ]),
-      transition('closed => open', [
-        animate('0s')
-      ])
-    ])
-  ]
+  styleUrls: ['./admin-panel.component.scss']
+  
 })
 export class AdminPanelComponent implements OnInit {
 
